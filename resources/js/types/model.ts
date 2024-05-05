e36470d85aa8ce48d5a603f35b5b0259ff40a83e
NaoyaMiyagawa <tsuna2764@gmail.com>
2024-05-05 16:55:09 +0800

    chore: intall breeze with inertia,vue,typescript

--- resources/js/types/model.ts
+++ resources/js/types/model.ts
@@ -1,28 +1,8 @@
 export type User = {
     id: number;
-    name: any;
-    email: any;
-    email_verified_at?: any;
-    created_at?: any;
-    updated_at?: any;
-    posts?: Post[];
-    latest_post?: Post;
-    latest_published_post?: Post;
+    name: string;
+    email: string;
+    email_verified_at?: string;
+    created_at?: string;
+    updated_at?: string;
 };
-export type Post = {
-    id: any;
-    title: any;
-    content?: string;
-    tags?: any;
-    metadata?: any;
-    status: PostStatus;
-    user_id: number;
-    created_at?: any;
-    updated_at?: any;
-    user?: User;
-};
-export enum PostStatus {
-    Draft = "draft",
-    Published = "published",
-    Unpublished = "unpublished"
-}
